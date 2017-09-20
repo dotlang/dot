67e774b1e7b7a0b1e4f07a5867e169fdd2e789f0
Mahdi Mohammadi <mahdix@gmail.com>
2017-09-20 06:00:33 +0100

    lexer - ignore comments till end of the line

--- src/dot.c
+++ src/dot.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+enum TOKEN_ID
 {
-    printf("Hello World!");
+    INCOMPLETE,
+    OTHER
+};
+
+int getTokenId(char* token, char next_char)
+{
+    return INCOMPLETE;
+}
+
+int main(int argc, char** argv)
+{
+	FILE *file;
+
+    file = fopen(argv[1], "r");
+    char token[256];
+    int token_length = 0;
+    memset(token, 0, 256);
+
+    for(;;) 
+    {
+      char c = (char)fgetc(file);
+	  if ( c == EOF ) break;
+	  if ( c == '#' )
+      {
+          char temp = c;
+          while ( temp != EOF && temp != '\n' ) temp = (char)fgetc(file);
+          continue;
+      }
+
+	  int token_id = getTokenId(token, c);
+	  printf("%c", c);
+    }
+
+    fclose(file);
+
+    return 219;
 }
