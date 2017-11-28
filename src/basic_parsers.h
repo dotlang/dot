fe1f9cb4d05aeec454717cbc468622c6da9fb98a
Mahdi Mohammadi <mahdix@gmail.com>
2017-11-28 05:36:36 +0000

    separate code into multiple source files

--- src/basic_parsers.h
+++ src/basic_parsers.h
@@ -2,114 +2,11 @@
 #define __BASIC_PARSERS_H__
 
 #include "ast.h"
-#include "debug_helpers.h"
 
-void ignoreWhitespace(Context* context)
-{
-    char c = (char)fgetc(context->input_file);
-    char temp[1024];
-
-    while ( isspace(c) || c == '#' ) 
-    {
-        if ( c == EOF ) break;
-        if ( c == '#' ) fgets(temp, 1024, context->input_file);
-
-        c = (char)fgetc(context->input_file);
-    }
-
-    ungetc(c, context->input_file);
-}
-
-//Try to read any of given characters. Return index of matching character
-int parseMultipleChoiceLiteral(Context* context, const char* choices)
-{
-    ignoreWhitespace(context);
-
-    SAVE_POSITION;
-    char c = (char)fgetc(context->input_file);
-    int choice_count = strlen(choices);
-
-    for(int i=0;i<choice_count;i++)
-    {
-        if ( c == choices[i] ) return i;
-    }
-
-    RESTORE_POSITION;
-    return FAIL;
-}
-
-int parseLiteral(Context* context, const char* literal)
-{
-    ignoreWhitespace(context);
-
-    SAVE_POSITION;
-    for(size_t i=0;i<strlen(literal);i++)
-    {
-        char c = (char)fgetc(context->input_file);
-        if ( c != literal[i] ) 
-        {
-            RESTORE_POSITION;
-            return FAIL;
-        }
-    }
-
-    return OK;
-}
-
-int parseNumber(Context* context, char* token)
-{
-    ignoreWhitespace(context);
-    SAVE_POSITION;
-
-    int token_len = 0;
-
-    char c = (char)fgetc(context->input_file);
-    if ( isdigit(c) )
-    {
-        while ( c != EOF && isdigit(c) ) 
-        {
-            token[token_len++] = c;
-            c = (char)fgetc(context->input_file);
-        }
-        ungetc(c, context->input_file);
-    }
-    else
-    {
-        RESTORE_POSITION;
-        return FAIL;
-    }
-
-    token[token_len] = 0;
-    return OK;
-}
-
-int parseIdentifier(Context* context, char* token)
-{
-    ignoreWhitespace(context);
-
-    SAVE_POSITION;
-    char c = (char)fgetc(context->input_file);
-    int token_len = 0;
-
-    if ( isalpha(c) )
-    {
-        while ( c != EOF && isalpha(c) )
-        {
-            token[token_len++] = c;
-            c = (char)fgetc(context->input_file);
-        }
-        ungetc(c, context->input_file);
-    }
-    else
-    {
-        RESTORE_POSITION;
-        return FAIL;
-    }
-
-    token[token_len] = 0;
-    debugLog(context, "Parsed an identifier: %s", token);
-        
-    return OK;
-}
+void ignoreWhitespace(Context* context);
+int parseMultipleChoiceLiteral(Context* context, const char* choices);
+int parseLiteral(Context* context, const char* literal);
+int parseNumber(Context* context, char* token);
+int parseIdentifier(Context* context, char* token);
 
 #endif
---
+++ src/parsers.c
@@ -0,0 +1,114 @@
+#include <string.h>
+
+
+#include "parsers.h"
+#include "ast.h"
+#include "basic_parsers.h"
+#include "debug_helpers.h"
+
+int parseExpression(Context*, Expression*);
+
+//MathFactor         = "(" Expression ")" | NUMBER
+int parseMathFactor(Context* context, MathFactor* factor)
+{
+    int result = parseLiteral(context, "(");
+    if ( result != FAIL )
+    {
+        factor->expression = ALLOC(Expression);
+        result = parseExpression(context, factor->expression);
+        if ( result == FAIL ) return FAIL;
+
+        result = parseLiteral(context, ")");
+        if ( result == FAIL ) return FAIL;
+
+        return OK;
+    }
+
+    char num1[16];
+    result = parseNumber(context, num1);
+    if ( result != FAIL )
+    {
+        factor->number = atoi(num1);
+        return OK;
+    }
+
+    return OK;
+}
+
+//MathExpression     = MathFactor ("+"|"-"|"*"|"/"|"%"|"%%") MathExpression | MathFactor
+int parseMathExpression(Context* context, MathExpression* exp)
+{
+    exp->factor = ALLOC(MathFactor);
+    int result = parseMathFactor(context, exp->factor);
+    if ( result == FAIL ) return FAIL;
+
+    const char* ops="+-*/";
+    result = parseMultipleChoiceLiteral(context, ops);
+
+    if ( result == FAIL ) {
+        exp->op = 0;
+        return OK;
+    }
+
+    exp->op = ops [result];
+    
+    exp->expression = ALLOC(MathExpression);
+    result = parseMathExpression(context, exp->expression);
+    if ( result == FAIL ) return FAIL;
+
+    return OK;
+}
+
+//Expression         = MathExpression 
+int parseExpression(Context* context, Expression* exp)
+{
+    exp->math_expression = ALLOC(MathExpression);
+    int result = parseMathExpression(context, exp->math_expression);
+
+    if ( result == FAIL ) return FAIL;
+    debugExpression(context, exp);
+    debugLog(context,"");
+
+    return OK;
+}
+
+//StaticBinding  = BindingLhs+ ":=" ( FunctionDecl )
+//FunctionDecl   = "(" ") ->" Expression
+int parseStaticBinding(Context* context, StaticBinding* b)
+{
+    char token[256];
+    int result = parseIdentifier(context, token);
+    if ( result == FAIL ) return FAIL;
+    strcpy(b->lhs, token);
+
+    result = parseLiteral(context, ":=");
+    if ( result == FAIL ) return FAIL;
+
+    result = parseLiteral(context, "()");
+    if ( result == FAIL ) return FAIL;
+
+    result = parseLiteral(context, "->");
+    if ( result == FAIL ) return FAIL;
+
+    b->function_decl = ALLOC(FunctionDecl);
+    b->function_decl->expression = ALLOC(Expression);
+
+    result = parseExpression(context, b->function_decl->expression);
+    if ( result == FAIL ) return FAIL;
+
+    return OK;
+}
+
+//Module            = { ( StaticBinding ) }
+int parseModule(Context* context, Module* module)
+{
+    StaticBinding* binding = ALLOC(StaticBinding);
+    ModuleItem* item = ALLOC(ModuleItem);
+    item->static_binding = binding;
+
+    module->items_head = module->items_tail = item;
+    int result = parseStaticBinding(context, binding);
+
+    return result;
+}
+
